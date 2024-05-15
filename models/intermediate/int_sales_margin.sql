42fb96ccdc8cb4b0ca75e74db407f129c67b6f4f
Ana Gama <anagama.analyst@gmail.com>
2024-05-15 15:39:04 +0000

    Orders margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,10 +1,12 @@
 SELECT
-products_id
+orders_id
+,products_id
 ,date_date
 ,revenue
 ,quantity
 ,CAST(purchase_price as FLOAT64) as purchase_price
 ,ROUND((quantity * purchase_price),2) as purchase_cost
+,ROUND(revenue - quantity * CAST(purchase_price as FLOAT64),2) as margin
 FROM {{ref("stg_raw__sales")}} sales
 LEFT JOIN {{ref("stg_raw__product")}} product
 USING (products_id) 
